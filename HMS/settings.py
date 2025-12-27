ca60d73710fcdd64f1cb96b84f86aeb45168b622
naresh1730 <surepallinaresh1730@gmail.com>
2025-12-27 17:22:38 +0530

    Change STATIC_ROOT to point to 'static' directory

--- HMS/settings.py
+++ HMS/settings.py
@@ -133,7 +133,7 @@ SITE_ID=1
 STATIC_URL = '/static/'
 # STATIC_ROOT='static/'
 #production
-STATIC_ROOT=BASE_DIR / 'staticfiles'
+STATIC_ROOT=BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
